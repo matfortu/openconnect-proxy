b0d45dd82b486d15b58c042697ffe22b692076fc
Mattia Fortunato <matfortu@gmail.com>
2023-03-01 10:26:59 +0100

    Update entrypoint.sh

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -7,7 +7,7 @@ ssh-keygen -A
 sed "s/^Port .*$/Port 8888/" -i /etc/tinyproxy.conf
 /usr/bin/tinyproxy -c /etc/tinyproxy.conf
 
-/usr/local/bin/microsocks -i 0.0.0.0 -p 8889 & 
+/usr/local/bin/microsocks -i 0.0.0.0 -p 8889 &
 
 run () {
   # Start openconnect
@@ -19,7 +19,7 @@ run () {
     (echo $OPENCONNECT_PASSWORD; echo $OPENCONNECT_MFA_CODE) | openconnect -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
   elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]] && [[ ! -z "${OPENCONNECT_TOTP_SECRET}" ]]; then
   # Time-based One Time Password (TOTP, "Google Authenticator")
-    OPENCONNECT_TOTP=$(oathtool -b --totp "$OPENCONNECT_TOTP_SECRET")
+    OPENCONNECT_TOTP=$(oathtool --time-step-size 60s --digits 6 --totp=SHA1 --base32 "$OPENCONNECT_TOTP_SECRET")
     echo -e "$OPENCONNECT_TOTP\n$OPENCONNECT_PASSWORD\n" | openconnect -u $OPENCONNECT_USER $OPENCONNECT_OPTIONS --passwd-on-stdin $OPENCONNECT_URL
   elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]]; then
   # Standard authentication
@@ -31,4 +31,3 @@ until (run); do
   echo "openconnect exited. Restarting process in 20 seconds…" >&2
   sleep 20
 done
-
